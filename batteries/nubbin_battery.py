dfcd325a5aa38ae7d576c22da383593809eac8bc
Luke <lbmoogs@gmail.com>
2023-11-12 19:02:42 +0000

    add: Create directory and python files for batteries

---
+++ batteries/nubbin_battery.py
@@ -0,0 +1,11 @@
+from battery import Battery
+from datetime import timedelta
+
+
+class NubbinBattery(Battery):
+    def __init__(last_service_date, current_date):
+        self.last_service_date = last_service_date
+        self.current_date = current_date
+
+    def needs_service(self):
+        return self.current_date - self.last_service_date > timedelta(years = 4)
