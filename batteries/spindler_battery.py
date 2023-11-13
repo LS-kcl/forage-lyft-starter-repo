15913905482583b35335670f1082defe3973248e
Luke <lbmoogs@gmail.com>
2023-11-13 13:41:33 +0000

    refactor: Change service date checking to avoid requiring additional timedelta package

--- batteries/spindler_battery.py
+++ batteries/spindler_battery.py
@@ -1,11 +1,11 @@
 from battery import Battery
-from datetime import timedelta
+from datetime import datetime
 
 
 class SpindlerBattery(Battery):
-    def __init__(last_service_date, current_date):
+    def __init__(last_service_date):
         self.last_service_date = last_service_date
-        self.current_date = current_date
 
     def needs_service(self):
-        return self.current_date - self.last_service_date > timedelta(years = 2)
+        service_threshold_date = self.last_service_date.replace(year=self.last_service_date.year + 2)
+        return service_threshold_date < datetime.today().date()
